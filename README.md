8603b3c26b4ee58803569e0343989e3be67ab5dd
Saul Shanabrook <s.shanabrook@gmail.com>
2017-08-28 12:50:19 -0400

    Got working with files

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # jupyterlab_voyager
 
-A JupyterLab MIME renderer extension to view data in the datavoyager.
+A JupyterLab MIME renderer extension to view CSV and JSON data in [Voyager 2](https://github.com/vega/voyager#voyager-2).
 
+![Screen shot showing data file opened in Voyager in JupyterLab](./screen-shot.png)
 
 ## Prerequisites
 
@@ -13,19 +14,31 @@ A JupyterLab MIME renderer extension to view data in the datavoyager.
 jupyter labextension install jupyterlab_voyager
 ```
 
+Then right click on any `csv` or `json` file click "Open with...", then "Voyager".
+
 ## Development
 
-For a development install (requires npm version 4 or later), do the following in the repository directory:
+For a development install (requires npm version 4 or later and yarn), do the following in the repository directory:
 
 ```bash
-npm install
+yarn install
 jupyter labextension link .
 ```
 
 To rebuild the package and the JupyterLab app:
 
 ```bash
-npm run build
+yarn build
 jupyter lab build
 ```
 
+## Future Work
+
+This extension provides a bare minimum integration with Voyager. It would be great to
+support features like:
+
+* Save Voyager state in widget, so that when window is reloaded it will preserve what
+  you have selected.
+* Allow viewing Pandas dataframes in the notebook with Voyager.
+
+*Created using [JupyterLab `extension-cookiecutter-ts`](https://github.com/jupyterlab/extension-cookiecutter-ts)*.
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+///<reference path="./lib.d.ts"/>
 import {
   IRenderMime
 } from '@jupyterlab/rendermime-interfaces';
@@ -6,61 +7,75 @@ import {
   Widget
 } from '@phosphor/widgets';
 
-
-/**
- * The default mime type for the extension.
- */
-const MIME_TYPE = 'application/csv';
-
-
-/**
- * The class name added to the extension.
- */
-const CLASS_NAME = 'jp-OutputWidgetCSV';
+import {CreateVoyager} from 'datavoyager/build/lib-voyager';
+import {VoyagerConfig} from 'datavoyager/build/models/config';
+import 'datavoyager/build/style.css';
+import {read} from 'vega-loader';
 
 
-/**
- * A widget for rendering CSV.
- */
-export
+const config: VoyagerConfig  = {
+  // don't allow user to select another data source from Voyager UI
+  showDataSourceSelector: false
+}
+
+// We use vega-loader.read to parse the text into what vega needs,
+// because this is what voyager does:
+// https://github.com/vega/voyager/blob/6a02e85906956b811a1003c3ad299368d2b33a67/src/components/data-selector/index.tsx#L167
+//
+// So, we need to match up JupyterLab mimetypes with the format types
+// for vega
+const mimeTypesToVegaTypes: {[key: string]: string} = {
+  'application/json': 'json',
+  'text/csv': 'csv'
+}
+
+
 class OutputWidget extends Widget implements IRenderMime.IRenderer {
-  /**
-   * Construct a new output widget.
-   */
   constructor(options: IRenderMime.IRendererOptions) {
     super();
     this._mimeType = options.mimeType;
-    this.addClass(CLASS_NAME);
   }
 
-  /**
-   * Render CSV into this widget's node.
-   */
   renderModel(model: IRenderMime.IMimeModel): Promise<void> {
-    this.node.textContent = JSON.stringify(model.data[this._mimeType]);
-    return Promise.resolve(void 0);
+    const data = model.data[this._mimeType] as string;
+    if (data === "") {
+      // it is originally rendered a bunch of times with empty
+      // data for some reason.
+      return Promise.resolve(undefined); 
+    }
+    try {
+      const type = mimeTypesToVegaTypes[this._mimeType];
+      const values = read(data, {type});
+      // it might be better to create a voyager instance once in the constructor,
+      // then just call update data here. 
+      CreateVoyager(this.node, config, {values})
+    } catch (e) {
+      this.node.textContent = `Failed to load file into Voyager: ${e}`;
+      throw e;
+    }
+    return Promise.resolve(undefined);     
   }
 
   private _mimeType: string;
 }
 
 
-/**
- * A mime renderer factory for CSV data.
- */
-export
-const rendererFactory: IRenderMime.IRendererFactory = {
-  safe: true,
-  mimeTypes: [MIME_TYPE],
-  createRenderer: options => new OutputWidget(options)
-};
+// create a new widget factory for each file type, so that the render factory get's a different
+// mime type for CSV and JSON
+const fileTypes = ['csv', 'json'];
 
-
-const extension: IRenderMime.IExtension = {
-  name: 'CSV',
-  rendererFactory,
-  rank: 0,
-  dataType: 'string'
-};
-
-export default extension;
+export default {
+  name: 'I don\'t know what this does',
+  rendererFactory: {
+    safe: false,
+    mimeTypes: Object.keys(mimeTypesToVegaTypes),
+    createRenderer: options => new OutputWidget(options)
+  },
+  dataType: 'string',
+  documentWidgetFactoryOptions: fileTypes.map((primaryFileType) => ({
+    // name needs to be different or else there is an error
+    name: `Voyager (${primaryFileType})`,
+    primaryFileType,
+    fileTypes: [primaryFileType]
+  }))
+} as IRenderMime.IExtension;
